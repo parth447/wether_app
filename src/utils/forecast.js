89fcbf7f01d884057eab15cc38b142beea07a1c9
parth447 <parth.prajapati92@gmail.com>
2019-07-06 19:29:23 +0530

    add some text in about and informtion in forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,9 +10,11 @@ const forecast = (lat, lang, callback) => {
             callback('unable to find location.Try another search.',undefined)
         }else{
             callback(undefined,"\n" + body.daily.data[0].summary + "\n its currently " + body.currently.temperature + " degree out.There is a " +
-            body.currently.precipProbability + "% chance for rain.")
+            body.currently.precipProbability + "% chance for rain."+"\n Today's high temperature is "+body.daily.data[0].temperatureHigh +" degree, and low temperature is "
+            +body.daily.data[0].temperatureLow +" degree.") 
         }
     })
+    console.log(url)
 }
 
 module.exports = forecast
